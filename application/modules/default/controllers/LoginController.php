acf0dce45a3ffbe32ef2450fdb591029e53a8e66
rik6167 <gps6167@gmail.com>
2014-08-19 16:16:53 +1000

    forget passord code

--- application/modules/default/controllers/LoginController.php
+++ application/modules/default/controllers/LoginController.php
@@ -122,4 +122,126 @@ class LoginController extends Zend_Controller_Action {
 		session_destroy ();
 		$this->_redirect ( 'login' );
 	}
+        
+                function forgetAction(){
+            $this->_helper->layout->setLayout ( 'layout_forget' );
+            
+        }
+        
+        function forgetpostAction(){
+            
+            # this function is bit complicated as it's changing password both for participants and Admin. 
+            # first check if based on URL. If it's an admin's url then it is looking only in the user table
+            # else it is looking in the participants table
+            # and generating random 8 chars string and sending email
+            // NEED TO PUT THE ADMIN URL IN THE DATABASE TOO TO MAKE IT DYNAMIC WHICH IS USEFUL FOR STAGING SITES ETC.
+            
+            
+            $this->_helper->viewRenderer->setNoRender ( true );
+            $this->_helper->layout->disableLayout ();
+            $f = new Zend_Filter_StripTags ();
+            $email = $f->filter ( $this->_request->getParam ( 'idval' ) );
+            $dbtable = $f->filter ( $this->_request->getParam ( 'tabla' ) );
+            $dbfiled = $f->filter ( $this->_request->getParam ( 'idname' ) );
+            
+            $objModel = new Default_Model_Generico ();
+            
+            if ($this->getRequest ()->isPost ()) {
+                $this->_db = Zend_Controller_Front::getInstance ()->getParam ( "bootstrap" )->getResource ( "db" );
+                
+                
+                // update table with new password    
+                $password = md5($this->rand_string(8));          
+                $values = array('password'=>$password);
+                
+                $domain_name = $_SERVER['HTTP_HOST'];
+                $client_id = $_POST ['client_id'];
+                #if client id is 1; then its admin
+                if($client_id == '1')
+                $isadmin = true;                 
+                if($isadmin){         
+                    $q = $objModel->getRows("id_client= '{$_POST ['client_id']}' AND user= '{$_POST ['email']}'",'user_login');
+                    if(count($q)== 1){                        
+                        $this->_db->update ( 'user', $values, 'id=' . $q[0]['id'] ); 
+                        
+                    }  else {
+                        echo 0; die();
+                    }
+    
+                  
+                }else{
+
+                    #if not admin
+                   
+//                    print_r($_POST);
+                    $q = $objModel->getRows("id_client= '{$_POST ['client_id']}' AND user= '{$_POST ['email']}' AND (id_licence = '0' OR id_licence = '{$_POST ['id_licence']}')",'user_login');
+                    
+                    # if record doesn't exist for this licence; show error.
+                    if(count($q)==0){echo 0;die();}
+                    
+                    if(!isset($_POST['atype'])){
+                       // email address return only one row, that is if licence is not 0 and client id exist
+
+                       # check for paricipant
+
+                       # if email address return 1 row 
+                       if(count($q)==1){
+                           #if licence number is 0 
+
+                           if($q[0]['id_licence']==0){
+                                # then its a client and we need to update user table
+                                $this->_db->update ( 'user', $values, 'id' . '=' . $q[0]['id'] );      
+                                
+                           }
+                           else{
+                                # else it if licence is not zeto then its particiant and updade participant table
+                                $this->_db->update ( 'program_participants', $values, 'id_participant' . '=' . $q[0]['id'] );    
+                                
+                           }
+                        }
+
+                       # if email address return 2 row 
+                       if(count($q)==2){
+                           #this is client; ask him which password to update. 
+                           echo '2';
+                           die();
+
+                        }
+                    }else{
+                        #if a type is participant then update participant else update client 
+                        if($_POST['atype']=='participant'){
+                            $q = $objModel->getRows("id_client= '{$_POST ['client_id']}' AND user= '{$_POST ['email']}' AND id_licence = '{$_POST ['id_licence']}'",'user_login');                            
+                            $this->_db->update ( 'program_participants', $values, 'id_participant' . '=' . $q[0]['id'] );  
+                            
+                        }else{
+                            $q = $objModel->getRows("id_client= '{$_POST ['client_id']}' AND user= '{$_POST ['email']}' AND id_licence = '0'", 'user_login');
+                            $this->_db->update ( 'user', $values, 'id' . '=' . $q[0]['id'] );    
+                            
+                        }
+                    }
+
+                }
+                
+                     #send email with password
+                    try{                    
+                        $mail = new Zend_Mail();
+                        $body = "<p>Hello : {$q [0]['firstname']}, <br/>Your<strong>New Password</strong>is:  {$password}</p><br/><p>You can upadte this by visting your profile at any time.";
+                        $mail->setBodyHtml($body)
+                             ->setFrom('motive8@ad-inc.com.au','motive8')
+                             ->addTo($_POST ['email'],"{$q [0]['firstname']}")
+                             ->setSubject('New password for Motive 8')
+                             ->send();
+                    }  catch (Zend_Mail_Transport_Exception $e){
+                        $e->getMessage();
+                    }
+                    echo 1;
+            }
+            
+           
+        }
+        
+        function rand_string( $length ) {
+            $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return substr(str_shuffle($chars),0,$length);
+        }
 }
